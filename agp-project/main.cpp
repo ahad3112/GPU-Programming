3282022f388f653d45cf63beb7205e8b12781b84
Rohin Mohanadas <rohin@kth.se>
2017-12-11 17:16:37 +0100

    initial collidor positions

--- agp-project/main.cpp
+++ agp-project/main.cpp
@@ -37,7 +37,8 @@ enum class CollidorEnum { ONE, TWO};
  * Unit of Distance : Km
  * Unit of Time : s
  */
-unsigned int NUM_PARTICLES 	= 10000;	        // total no. of particles
+unsigned int NUM_PARTICLES 	= 30000;	        // total no. of particles
+float PARTICLE_ZOOM         = 3.5f;           // for scaling
 unsigned int NUM_IT	        = 100;			// total number of iteration
 float PERCENT_IRON          = 0.30f;        // percentage of iron in a collidor
 float g_diameter 	= 376.78f;
@@ -57,8 +58,8 @@ float g_time_step	= 5.8117f;
  * Other constants
  * (Sourced from Internet)
  * */
+float g_radius_collidor  = 25576.86545f;
 //float g_radius_earth  = 6371;
-float g_radius_earth  = 6371;
 float g_radius_core_fe;
 
 
@@ -69,7 +70,7 @@ Display                 *dpy;
 XVisualInfo             *vi;
 GLint                   att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+// void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 struct floats {
 
@@ -106,9 +107,12 @@ Particle* particles2;
 
 // variable related to user interaction
 float thetaZ = 0;
-float circleRadius = 90000.0f;
+float CAM_RADIUS = 105000.0f;
 float fov = 45.0f;
-
+float MIN_ZOOM = 5000.0f;
+float MAX_ZOOM = 9000000.0f;
+float ZOOM_STEP = 1000.0f;
+float TIME_STEP = 4.0f;
 
 
 float3 g_center_mass_one = float3(23925.0f,0.0f,9042.7f);
@@ -161,7 +165,7 @@ float3 randomSphere(float3 rho){
 /**
  * Uses a random spherical distribution to create random locations in a shell of
  * inner radius : g_radius_core_fe
- * outer radius : g_radius_earth
+ * outer radius : g_radius_collidor
  * returns : a random position in the shell
  * */
 float3 randomShell(float3 rho){
@@ -172,7 +176,7 @@ float3 randomShell(float3 rho){
     float mu    = (1 - 2 * rho.y);
     float angle = 2*PI*rho.z;
 
-    pre     = std::cbrt(std::pow(g_radius_core_fe,3) + (std::pow(g_radius_earth,3) - std::pow(g_radius_core_fe,3)) * rho.x);
+    pre     = std::cbrt(std::pow(g_radius_core_fe,3) + (std::pow(g_radius_collidor,3) - std::pow(g_radius_core_fe,3)) * rho.x);
     suf     = std::sqrt(1 - std::pow(mu,2));
     res.x   = pre * suf * std::cos(angle);
     res.y   = pre * suf * std::sin(angle);
@@ -209,12 +213,12 @@ float3 computeVelocity(float3 position, CollidorEnum value){
 
 	return velocity;
 }
+
 /**
  * This method initialized the list of particles (random for now...!!!!!!!!)
  **/
 Particle* generateNewParticles(){
 	// malloc should be replaced for page-locked memory
-	//Particle* particles = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES); // non-pinned memory
 
     /**
      * Compute the radiue of the Fe core and Si Shell
@@ -223,21 +227,17 @@ Particle* generateNewParticles(){
      * 2. Radius of earth
      * */
 
-    g_radius_core_fe = g_radius_earth * std::cbrt(0.3);
+    g_radius_core_fe = g_radius_collidor * std::cbrt(0.3);
 
-	//Particle* particles_impone;
-	// Particle* particles_imptwo;
-	// page-locked (pinned) memory
-	//CUDA_CHECK_RETURN(cudaHostAlloc((void**)&particles, NUM_PARTICLES * sizeof(Particle) , cudaHostAllocDefault));
 
-  Particle* particles_impone = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES);
+    Particle* particles_impone = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES);
 
-    int num_iron_particles = (int) (PERCENT_IRON * NUM_PARTICLES);
-    int num_silica_particles = NUM_PARTICLES - num_iron_particles;
+    unsigned int num_iron_particles = (unsigned int) (PERCENT_IRON * NUM_PARTICLES);
+    //unsigned int num_silica_particles = NUM_PARTICLES - num_iron_particles;
 
     // initialize iron particle positions
     float3 rho;
-    int i=0;
+    unsigned int i=0;
 	for(i = 0; i< num_iron_particles; i++){
     rho = rndFloat3();
 		float3 position = randomSphere(rho);
@@ -262,7 +262,6 @@ Particle* generateNewParticles(){
 
 Particle* generateNewParticles2(){
 	// malloc should be replaced for page-locked memory
-	//Particle* particles = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES); // non-pinned memory
 
     /**
      * Compute the radiue of the Fe core and Si Shell
@@ -271,21 +270,15 @@ Particle* generateNewParticles2(){
      * 2. Radius of earth
      * */
 
-    g_radius_core_fe = g_radius_earth * std::cbrt(0.3);
+    g_radius_core_fe = g_radius_collidor * std::cbrt(0.3);
 
-	//Particle* particles_impone;
-	// Particle* particles_imptwo;
-	// page-locked (pinned) memory
-	//CUDA_CHECK_RETURN(cudaHostAlloc((void**)&particles, NUM_PARTICLES * sizeof(Particle) , cudaHostAllocDefault));
+    Particle* particles_imptwo = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES);
 
-  Particle* particles_imptwo = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES);
-
-    int num_iron_particles = (int) (PERCENT_IRON * NUM_PARTICLES);
-    int num_silica_particles = NUM_PARTICLES - num_iron_particles;
+    unsigned int num_iron_particles = (unsigned int) (PERCENT_IRON * NUM_PARTICLES);
 
     // initialize iron particle positions
     float3 rho;
-    int i=0;
+    unsigned int i=0;
 	for(i = 0; i< num_iron_particles; i++){
     rho = rndFloat3();
 		float3 position = randomSphere(rho);
@@ -305,18 +298,6 @@ Particle* generateNewParticles2(){
 }
 
 
-// this method initialize and return a list of particles
-Particle* generateParticles(int n){
-  Particle* particles = (Particle*)malloc(sizeof(Particle) * n);
-  for(int i = 0; i < n; i++){
-    particles[i].position = glm::vec3(getRND(MIN,MAX),getRND(MIN,MAX),getRND(MIN,MAX));
-    particles[i].velocity = glm::vec3(getRND(MIN,MAX),getRND(MIN,MAX),getRND(MIN,MAX));
-    //particles[i].radius = getRND(.25f,0.3f);
-    particles[i].radius = .10f;
-  }
-  return particles;
-}
-
 // This method initialize the 3D viewglfw3
 void init()
 {
@@ -334,7 +315,7 @@ void init()
     glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // initial view and projection matrices
-    view  = glm::lookAt(glm::vec3(0.0f, 0.0f, 60000.0f),
+    view  = glm::lookAt(glm::vec3(0.0f, 0.0f, CAM_RADIUS),
       glm::vec3(0.0f, 0.0f, 0.0f),
       glm::vec3(0.0f, 1.0f, 0.0f));
     projection = glm::perspective(glm::radians(fov), (float)WIDTH / (float)HEIGHT, 1000.0f, 100000.0f);
@@ -366,17 +347,19 @@ void display(GLFWwindow *window)
 
         // need to activate the shader before any calls to glUniform
         glUseProgram(shaderProgram);
-            // Identity matrix
-        //printf("%f %f %f\n", particles[i].position.x, particles[i].position.y, particles[i].position.z);
-        //model = glm::translate(model, glm::vec3(100000.0f,100.0f,100.0f));
 
-        for(int i = 0; i< NUM_PARTICLES;i++){
+        /**
+        * Collidor 1:
+        *
+        **/
+
+        for(uint i = 0; i< NUM_PARTICLES;i++){
           // create transformation matrices+glm::translate(model, particles2[i].position)
           // model = glm::translate(model, particles2[i].position);
           //model = glm::translate(model, glm::vec3(0.0f,0.0f,0.0f));
           // retrieve the matrix uniform locations
           model = glm::mat4(1.0f);
-          model = glm::translate(model,particles[i].position);
+          model = glm::scale(glm::translate(model,particles[i].position),glm::vec3( PARTICLE_ZOOM ));
 
           unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
           unsigned int viewLoc  = glGetUniformLocation(shaderProgram, "view");
@@ -401,54 +384,25 @@ void display(GLFWwindow *window)
 
           }
 
-
           // draw our transformed particle as a sphere
           glutSolidSphere(80.8f,10,10);
           //glutWireSphere(particles[i].radius,9,9);
 
           // // update position randomly
-          // particles[i].velocity = particles[i].velocity + glm::vec3(0.0f,-dt * g, 0.0f);
-          particles[i].position = particles[i].position + particles[i].velocity * 2.0f;
-          //particles2[i].position = particles2[i].position + particles2[i].velocity * 4.0f;
 
+          particles[i].position = particles[i].position + particles[i].velocity * TIME_STEP;
 
-          // //glm::vec3 perturbation = glm::vec3(getRND(MIN,MAX),getRND(MIN,MAX),getRND(MIN,MAX)) * SCALE;
-          // //particles[i].position = particles[i].position + perturbation;
-          //
-          // // Handling boundary condition
-          // bool hitBoundary = false;
-          // if(particles[i].position.x + particles[i].radius <= MIN || particles[i].position.x + particles[i].radius>= MAX){
-          //   particles[i].position.x = glm::min(glm::max(particles[i].position.x,MIN),MAX);
-          //   hitBoundary = true;
-          // }
-          // if(particles[i].position.y + particles[i].radius<= MIN || particles[i].position.y + particles[i].radius>= MAX){
-          //   particles[i].position.y = glm::min(glm::max(particles[i].position.y,MIN),MAX);
-          //   hitBoundary = true;
-          // }
-          // if(particles[i].position.z + particles[i].radius <= MIN || particles[i].position.z + particles[i].radius >= MAX){
-          //   particles[i].position.z = glm::min(glm::max(particles[i].position.z,MIN),MAX);
-          //   hitBoundary = true;
-          // }
-          //
-          // if(hitBoundary){
-          //   particles[i].velocity *= -1.0f;
-          // }
-
-          //particles[i].position.x = glm::min(glm::max(particles[i].position.x,MIN),MAX);
-          //particles[i].position.y = glm::min(glm::max(particles[i].position.y,MIN),MAX);
-          //particles[i].position.z = glm::min(glm::max(particles[i].position.z,MIN),MAX);
       }
 
-      //For the second particle rendering
-      for(int i = 0; i< NUM_PARTICLES;i++){
-        // create transformation matrices+glm::translate(model, particles2[i].position)
-        // model = glm::translate(model, particles2[i].position);
-        //model = glm::translate(model, glm::vec3(0.0f,0.0f,0.0f));
+      /**
+      * Collidor 2:
+      *
+      **/
+      for(uint i = 0; i< NUM_PARTICLES;i++){
+
         // retrieve the matrix uniform locations
         model = glm::mat4(1.0f);
-        model = glm::translate(model,particles2[i].position);
-
-
+        model = glm::scale(glm::translate(model,particles2[i].position),glm::vec3( PARTICLE_ZOOM ));
         unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
         unsigned int viewLoc  = glGetUniformLocation(shaderProgram, "view");
         unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
@@ -481,36 +435,8 @@ void display(GLFWwindow *window)
 
 
         // // update position randomly
-        // particles[i].velocity = particles[i].velocity + glm::vec3(0.0f,-dt * g, 0.0f);
-        //particles[i].position = particles[i].position + particles[i].velocity * 2.0f;
-        particles2[i].position = particles2[i].position + particles2[i].velocity * 4.0f;
+        particles2[i].position = particles2[i].position + particles2[i].velocity * TIME_STEP;
 
-
-        // //glm::vec3 perturbation = glm::vec3(getRND(MIN,MAX),getRND(MIN,MAX),getRND(MIN,MAX)) * SCALE;
-        // //particles[i].position = particles[i].position + perturbation;
-        //
-        // // Handling boundary condition
-        // bool hitBoundary = false;
-        // if(particles[i].position.x + particles[i].radius <= MIN || particles[i].position.x + particles[i].radius>= MAX){
-        //   particles[i].position.x = glm::min(glm::max(particles[i].position.x,MIN),MAX);
-        //   hitBoundary = true;
-        // }
-        // if(particles[i].position.y + particles[i].radius<= MIN || particles[i].position.y + particles[i].radius>= MAX){
-        //   particles[i].position.y = glm::min(glm::max(particles[i].position.y,MIN),MAX);
-        //   hitBoundary = true;
-        // }
-        // if(particles[i].position.z + particles[i].radius <= MIN || particles[i].position.z + particles[i].radius >= MAX){
-        //   particles[i].position.z = glm::min(glm::max(particles[i].position.z,MIN),MAX);
-        //   hitBoundary = true;
-        // }
-        //
-        // if(hitBoundary){
-        //   particles[i].velocity *= -1.0f;
-        // }
-
-        //particles[i].position.x = glm::min(glm::max(particles[i].position.x,MIN),MAX);
-        //particles[i].position.y = glm::min(glm::max(particles[i].position.y,MIN),MAX);
-        //particles[i].position.z = glm::min(glm::max(particles[i].position.z,MIN),MAX);
     }
 
 
@@ -528,20 +454,31 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     glfwSetWindowShouldClose(window, true);
   } else if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
     thetaZ -= 1.0f;
-    view = glm::lookAt(glm::vec3(circleRadius*sin(glm::radians(thetaZ)), 0.0f, circleRadius*cos(glm::radians(thetaZ))),
+    view = glm::lookAt(glm::vec3(CAM_RADIUS*sin(glm::radians(thetaZ)), 0.0f, CAM_RADIUS*cos(glm::radians(thetaZ))),
               glm::vec3(0.0f, 0.0f, 0.0f),
               glm::vec3(0.0f, 1.0f, 0.0f));
   } else if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
     thetaZ += 1.0f;
-    view = glm::lookAt(glm::vec3(circleRadius*sin(glm::radians(thetaZ)), 0.0f, circleRadius*cos(glm::radians(thetaZ))),
+    view = glm::lookAt(glm::vec3(CAM_RADIUS*sin(glm::radians(thetaZ)), 0.0f, CAM_RADIUS*cos(glm::radians(thetaZ))),
               glm::vec3(0.0f, 0.0f, 0.0f),
               glm::vec3(0.0f, 1.0f, 0.0f));
   }else if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
-    fov -= 1.0f;
-    projection = glm::perspective(glm::radians(fov), (float)WIDTH / (float)HEIGHT, 1000.0f, 100000.0f);
+
+    if(CAM_RADIUS-ZOOM_STEP <= MIN_ZOOM)
+      CAM_RADIUS = MIN_ZOOM;
+    else
+      CAM_RADIUS -= ZOOM_STEP;
+          view = glm::lookAt(glm::vec3(CAM_RADIUS*sin(glm::radians(thetaZ)), 0.0f, CAM_RADIUS*cos(glm::radians(thetaZ))),
+          glm::vec3(0.0f, 0.0f, 0.0f),
+          glm::vec3(0.0f, 1.0f, 0.0f));
   } else if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
-    fov += 1.0f;
-    projection = glm::perspective(glm::radians(fov), (float)WIDTH / (float)HEIGHT, 1000.0f, 100000.0f);
+    if(CAM_RADIUS-ZOOM_STEP >= MAX_ZOOM)
+      CAM_RADIUS = MAX_ZOOM;
+    else
+      CAM_RADIUS += ZOOM_STEP;
+          view = glm::lookAt(glm::vec3(CAM_RADIUS*sin(glm::radians(thetaZ)), 0.0f, CAM_RADIUS*cos(glm::radians(thetaZ))),
+          glm::vec3(0.0f, 0.0f, 0.0f),
+          glm::vec3(0.0f, 1.0f, 0.0f));
   }
 }
 
@@ -571,44 +508,6 @@ init();
 
 //Creating OpenGL context and binding it to window
 
-		dpy = XOpenDisplay(NULL);
-
-    if(dpy == NULL) {
-        printf("\n\tcannot connect to X server\n\n");
-        exit(0);
-      }
-
-    vi = glXChooseVisual(dpy, 0, att);
-		glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
-
-
-
-    //CUDA-OpenGL binding part
-
-	/*	glXMakeCurrent(dpy, win, glc);
-		glEnable(GL_DEPTH_TEST);
-
-
-//Creating OpenGL buffers
-
-		GLuint bufferID;
-// Generate a buffer ID
-		glGenBuffers(1,&bufferID);
-// Make this the current UNPACK buffer (OpenGL is state-based)
-
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, bufferID);
-	// Allocate data for the buffer
-		glBufferData(GL_PIXEL_UNPACK_BUFFER, WIDTH * HEIGHT * 4, NULL,GL_DYNAMIC_COPY);
-
-    cudaGLRegisterBufferObject( bufferID );
-
-*/
-    //map_buffer(bufferID);
-    framebuffer_size_callback(window,WIDTH,HEIGHT);
-
-
-
-
 while (!glfwWindowShouldClose(window))
 {
   display(window);
@@ -623,90 +522,14 @@ glfwTerminate();
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-  // make sure the viewport matches the new window dimensions; note that width and
-  // height will be significantly larger than specified on retina displays.
-  //std::cout<<"Entered here once!!";
-  glViewport(0, 0, width, height);
-  glLoadIdentity();
-  glOrtho(0,1.0f,0,1.0f,-1.0f,1.0f);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-}
-
-/*##############################################From the old git################################################### */
-
-// This methods return a random float number
-
-
-// float3 computeVelocity(float3 position, CollidorEnum value){
-//
-// 	float3 l_center_mass;
-// 	float3 l_linear_velocity;
-// 	float3 l_angular_velocity;
-// 	float3 velocity;
-// 	float r_xz;
-// 	float theta;
-// 	if(value == CollidorEnum::ONE){
-// 		 l_center_mass = g_center_mass_one;
-// 		 l_linear_velocity = g_linear_velocity_one;
-// 		 l_angular_velocity = g_angular_velocity_one;
-// 	}else{
-// 		 l_center_mass = g_center_mass_two;
-// 		 l_linear_velocity = g_linear_velocity_two;
-// 		 l_angular_velocity = g_angular_velocity_two;
-// 	}
-//
-// 	velocity = l_linear_velocity;
-// 	r_xz = std::sqrt(pow((position.x - l_center_mass.x),2) + pow((position.z - l_center_mass.z),2));
-// 	theta = atan2((position.z - l_center_mass.z)/(position.x - l_center_mass.x));
-// 	velocity.x += l_angular_velocity.y * r_xz * std::sin(theta);
-// 	velocity.y += - l_angular_velocity.y * r_xz * std::cos(theta);
-//
-// 	return velocity;
-// }
-//
-// /**
-//  * This method initialized the list of particles (random for now...!!!!!!!!)
-//  **/
-// Particle* init(){
-// 	// malloc should be replaced for page-locked memory
-// 	//Particle* particles = (Particle*)malloc(sizeof(Particle) * NUM_PARTICLES); // non-pinned memory
-//
-//     /**
-//      * Compute the radiue of the Fe core and Si Shell
-//      * Known factors
-//      * 1. Fe core composes of 30% and the shell constitutes the rest
-//      * 2. Radius of earth
-//      * */
-//
-//     g_radius_core_fe = g_radius_earth * std::cbrt(0.3);
-//
-// 	Particle* particles_impone;
-// 	// Particle* particles_imptwo;
-// 	// page-locked (pinned) memory
-// 	CUDA_CHECK_RETURN(cudaHostAlloc((void**)&particles, NUM_PARTICLES * sizeof(Particle) , cudaHostAllocDefault));
-//
-//     int num_iron_particles = (int) (PERCENT_IRON * NUM_PARTICLES);
-//     int num_silica_particles = NUM_PARTICLES - num_iron_particles;
-//
-//     // initialize iron particle positions
-//     float3 rho;
-//     int i=0;
-// 	for(i = 0; i< num_iron_particles; i++){
-//         rho = rndFloat3();
-// 		float3 position = randomSphere(rho);
-// 		position = position + g_center_mass_one;
-// 		float3 velocity = computeVelocity(position, CollidorEnum::ONE);
-// 		particles_impone[i] = Particle(position , velocity , ParticleType::IRON);
-//     }
-//     for(; i< NUM_PARTICLES;i++){
-//         rho = rndFloat3();
-// 		float3 position = randomShell(rho);
-// 		position = position + g_center_mass_one;
-// 		float3 velocity = computeVelocity(position, CollidorEnum::ONE);
-//         particles_impone[i] = Particle(position, velocity , ParticleType::SILICA);
-//     }
-// 	return particles;
+// void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+// {
+//   // make sure the viewport matches the new window dimensions; note that width and
+//   // height will be significantly larger than specified on retina displays.
+//   //std::cout<<"Entered here once!!";
+//   glViewport(0, 0, width, height);
+//   glLoadIdentity();
+//   glOrtho(0,1.0f,0,1.0f,-1.0f,1.0f);
+//   glMatrixMode(GL_MODELVIEW);
+//   glLoadIdentity();
 // }
